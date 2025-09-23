6585ad6ac1fa3d24356f9eecf5918aecd39214f1
SUNWOOYOUNGMIN <swym56123@naver.com>
2025-09-23 23:56:27 +0900

    Title: 시험 통과 여부 확인하기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250923/시험 통과 여부 확인하기/verify-test-passed.java
+++ 250923/시험 통과 여부 확인하기/verify-test-passed.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        if(n>-80){
+        if(n>=80){
             System.out.print("pass");
         } else {
             System.out.printf("%d more score",80-n);
