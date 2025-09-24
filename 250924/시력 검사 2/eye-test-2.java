3b86a3c8441cdba504c94ae333020bdedc3dd30e
SUNWOOYOUNGMIN <swym56123@naver.com>
2025-09-24 10:29:23 +0900

    Title: 시력 검사 2, Time: 88ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250924/시력 검사 2/eye-test-2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        double avg = Double.parseDouble(st.nextToken());
+
+        if(avg>1.0){
+            System.out.print("High");
+        } else if (avg>0.5){
+            System.out.print("Middle");
+        } else {
+            System.out.print("Low");
+        }
+    }
+}
