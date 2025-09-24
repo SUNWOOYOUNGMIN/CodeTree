9dd7f0bb14ecd264f0c3ce566228d0ffa37a5b80
SUNWOOYOUNGMIN <swym56123@naver.com>
2025-09-24 13:38:08 +0900

    Title: 알파벳에 따른 평가, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

--- 250924/알파벳에 따른 평가/evaluation-by-alphabet.java
+++ 250924/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        char a = sc.next()charAt(0);
+        char a = sc.next().charAt(0);
 
         if(a =='S'){
             System.out.print("Superior");
