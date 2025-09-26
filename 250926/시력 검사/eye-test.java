6efa4957e0f1ba42168ab02c59934e8097489aaa
SUNWOOYOUNGMIN <swym56123@naver.com>
2025-09-26 10:32:12 +0900

    Title: 시력 검사, Time: 140ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250926/시력 검사/eye-test.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        double right = sc.nextDouble();
+        double left = sc.nextDouble();
+
+        if(right>=1.0 && left>=1.0){
+            System.out.print("High");
+        } else if (right>=0.5 && left>=0.5){
+            System.out.print("Middle");
+        } else {
+            System.out.print("Low");
+        }
+    }
+}
