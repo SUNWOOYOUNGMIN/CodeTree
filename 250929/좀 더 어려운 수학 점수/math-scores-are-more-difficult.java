5b7d2d67b10ea71c416b9cf68b1d007361a939cf
SUNWOOYOUNGMIN <swym56123@naver.com>
2025-09-29 02:09:18 +0900

    Title: 좀 더 어려운 수학 점수, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250929/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a_math = sc.nextInt();
+        int a_eng = sc.nextInt();
+        int b_math = sc.nextInt();
+        int b_eng = sc.nextInt();
+
+        if (a_math > b_math){
+            System.out.print("A");
+        } else if (a_math < b_math) {
+            System.out.print("B");
+        } else {
+            if(a_eng > b_eng) {
+                System.out.print("A");
+            } else {
+                System.out.print("B");
+            }
+        }
+    }
+}
