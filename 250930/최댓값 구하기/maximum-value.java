ae511daee859eabbeef71bff0a37e2641c766f5f
SUNWOOYOUNGMIN <swym56123@naver.com>
2025-09-30 18:11:35 +0900

    Title: 최댓값 구하기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250930/최댓값 구하기/maximum-value.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(a<b){
+            if(c<b){
+                System.out.print(b);
+            } else {
+                System.out.print(c);
+            }
+        } else if (b<c){
+            if(a<c){
+                System.out.print(c);
+            } else {
+                System.out.print(a);
+            }
+        } else {
+            if(b<a){
+                System.out.print(a);
+            } else {
+                System.out.print(b);
+            }
+        }
+    }
+}
